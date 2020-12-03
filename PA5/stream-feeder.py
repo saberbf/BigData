4d00b88a5bd8af68d5d88d8e3b8de998e376a828
root <root@cluster-0098-m.us-east1-b.c.bigdata-09012020.internal>
2020-12-03 17:09:01 +0000

    dataset for PA5 is gathered

---
+++ PA5/stream-feeder.py
@@ -0,0 +1,25 @@
+import time, datetime
+import pandas as pd
+
+# Extracted from: https://www.nature.com/articles/s41599-020-0523-3#Sec7 [Supplementary information]
+dj30_df = pd.read_csv('../data/dj30.csv') 
+dj30_df.dropna(inplace=True)
+dj30_df = dj30_df.loc[:,['Date', 'Close']]
+dj30 = dj30_df['Close'].tolist()
+
+class CloseFeed:
+    source = None
+    def __init__(self, source):
+        self.source = source
+        
+    def getClose(self):
+        i = 0
+        while True:
+            time.sleep(0.25)
+            i += 1
+            yield self.source[i-1]
+
+dj30CloseFeed = CloseFeed(dj30)
+
+for close in dj30CloseFeed.getClose():
+    print (close)
